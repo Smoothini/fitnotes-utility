aa2cf70ba172581eeb0ab5ab2fad28dc9eb20e77
Smoothini <katona.andrei@gmail.com>
2026-03-31 02:09:53 +0200

    chore: removed useless pwa

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,3 @@
-// Check if the browser supports Service Workers
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
-
 let SQL;
 let currentDb = null;
 let originalFileArray = null;
